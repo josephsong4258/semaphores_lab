37cb54870e6502792017cde6bb4f3f8beb16a74a
josephsong4258 <josephsong44610@gmail.com>
2025-04-25 23:20:18 +0000

    a

--- barbarian.c
+++ barbarian.c
@@ -25,12 +25,18 @@ void barb_attack(int sig){
   }
 
 int main(void) {
-  // https://www.geeksforgeeks.org/posix-shared-memory-api/
+  // https://man7.org/linux/man-pages/man3/shm_open.3.html
   // shm_open gives a file descriptor that represents a chunk of shared memory managed by the OS
   // First parameter specifies name of shared-memory object - we got it from dungeon_info.h
   // Next parameter is if it hasn't been created yet(O_CREAT), or if it's open for reading/writing(O_RDWR)
   // Third parameter - 0666 seems to be the default value. Just tells us Owners, groups, and others have R/W permission
   int shm_fd = shm_open(dungeon_shm_name, O_RDWR, 0666);
+  // If successful, shm_open returns a file descriptor(non-negative integer)
+  // On failure, returns -1
+  if (shm_fd == -1) {
+    perror("Failed to open dungeon");
+    exit(1);
+  }
 
   // https://man7.org/linux/man-pages/man2/mmap.2.html
   // mmap maps the shared memory object into the process's virtual memory
@@ -41,5 +47,11 @@ int main(void) {
   // file descriptor is shm_fd
   // represents the offset - don't need one, so 0
   dungeon = mmap(0, sizeof(struct Dungeon), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  // if mapping is successful, mmap returns a pointer to the mapped area.
+  // If not successful, mmap returns the value MAP_FAILED( equal to (void *) -1)
+  if (dungeon == MAP_FAILED) {
+    perror("Failed to map dungeon");
+    exit(1);
+  }
   return 0;
       }
