9141c51f2a3a6fc419a3902a0c1337d5fe6ae3ad
josephsong4258 <josephsong44610@gmail.com>
2025-04-25 21:54:25 +0000

    Create files

--- game.c
+++ game.c
@@ -1,6 +1,14 @@
 #include "dungeon_info.h"
 #include "dungeon_settings.h"
+#include <unistd.h> //gets pid
 #include <stdio.h>
 
 //should be responsible for calling fork and exec
+// Once all characters have launched, call RunDungeon using the pid's of the character classes that you launched.
+//  If something isn't set up right, you will likely see an appropriate error.
 int main(void) {
+  pid_t barbarian_pid, wizard_pid, rogue_pid;
+  RunDungeon(wizard_pid, rogue_pid, barbarian_pid);
+
+  return 0;
+  }
