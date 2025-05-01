ba79880c982696038d37adf930643303067f8f9b
josephsong4258 <josephsong44610@gmail.com>
2025-05-01 23:10:48 +0000

    a

--- wizard.c
+++ wizard.c
@@ -1,8 +1,16 @@
 #include "dungeon_info.h"
 #include "dungeon_settings.h"
+#include <stddef.h>
 #include <unistd.h> //gets pid
 #include <stdio.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <sys/shm.h>
+#include <fcntl.h> //O_RDWR
+#include <sys/stat.h> // 0666
+#include <stdlib.h>
 
+struct Dungeon *dungeon;
 int main (void) {
   return 0;
   }
