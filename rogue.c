fe40185132a8591678c36e7b3fd58a140265be9c
josephsong4258 <josephsong44610@gmail.com>
2025-05-02 01:20:13 +0000

    a

--- rogue.c
+++ rogue.c
@@ -4,5 +4,30 @@
 #include <stdio.h>
 
 int main (void) {
-    return 0;
-}
+	// Same stuff - taken from barbarian.c
+  	int shm_fd = shm_open(dungeon_shm_name, 0_RDWR, 0666);
+	if (shm_fd == -1) {
+    	perror("Rogue failed to open dungeon");
+    	exit(1);
+	}
+	dungeon = mmap(0, sizeof(struct Dungeon), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  	if (dungeon == MAP_FAILED) {
+    	perror("Rogue failed to map dungeon");
+    	exit(1);
+  }
+	struct sigaction sa;
+  	sa.sa_handler = rogue_signal;
+  	sigemptyset(&sa.sa_mask);
+  	sa.sa_flags = 0;
+
+  	if (sigaction(DUNGEON_SIGNAL, &sa, 0) == -1) {
+    	perror("Rogue failed to set up signal");
+    	exit(1);
+    }
+
+   while (1){
+     pause();
+    }
+
+	return 0;
+  }
