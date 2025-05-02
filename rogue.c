a1c9454a7b789ae2076ef09840de03776786be32
josephsong4258 <josephsong44610@gmail.com>
2025-05-02 01:28:28 +0000

    a

--- rogue.c
+++ rogue.c
@@ -1,7 +1,23 @@
 #include "dungeon_info.h"
 #include "dungeon_settings.h"
+#include <stddef.h>
 #include <unistd.h> //gets pid
 #include <stdio.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <sys/shm.h>
+#include <fcntl.h> //O_RDWR
+#include <sys/stat.h> // 0666
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+struct Dungeon *dungeon;
+
+void rogue_signal(int sig) {
+
+}
+
 
 int main (void) {
 	// Same stuff - taken from barbarian.c
