5462b9729df95ca0b11475f45209cbcb861539f3
josephsong4258 <josephsong44610@gmail.com>
2025-05-02 02:34:33 +0000

    a

--- game.c
+++ game.c
@@ -5,7 +5,7 @@
 
 //should be responsible for calling fork and exec
 // Once all characters have launched, call RunDungeon using the pid's of the character classes that you launched.
-//  If something isn't set up right, you will likely see an appropriate error.
+//  If something isn't set up right, you will likely see an appropriate error
 int main(void) {
   // PID Represents the process ID - a unique identtifier for each process
   pid_t barbarian_pid, wizard_pid, rogue_pid;
